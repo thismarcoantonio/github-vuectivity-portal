05b5b8b327cddaa52ff32e544f871c67246c372d
thismarcoantonio <thismarcoantonio@gmail.com>
2020-02-13 02:53:35 -0300

    Add Ant Design to the project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue"
 import App from "@/App.vue"
 import store from "@/store"
+import AntDesign from "ant-design-vue"
+import "ant-design-vue/dist/antd.css"
 
 Vue.config.productionTip = false
+Vue.use(AntDesign)
 
 new Vue({
   store,
