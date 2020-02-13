194fdc339c70e0a1e3be59d8c6249b71b911ff3a
thismarcoantonio <thismarcoantonio@gmail.com>
2020-02-13 02:54:18 -0300

    Add basic ContributionCalendar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>New application</h1>
+  <a-row class="container">
+    <a-col :xs="{ span: 20 }" :lg="{ span: 19 }">
+      <contribution-calendar />
+    </a-col>
+    <a-col :xs="{ span: 2, offset: 2 }" :lg="{ span: 4, offset: 1 }">Right side</a-col>
+  </a-row>
 </template>
+
+<script>
+import ContributionCalendar from "@/components/ContributionCalendar"
+
+export default {
+  components: {
+    ContributionCalendar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    width: 95%;
+    max-width: 930px;
+    margin: 100px auto;
+  }
+</style>
